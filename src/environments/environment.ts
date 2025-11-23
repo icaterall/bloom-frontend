0ae2129496ba69888a688bfa51c36475e92abd28
icaterall <57697382+icaterall@users.noreply.github.com>
2025-11-23 09:29:39 +0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: false,
   hmr: false, 
   frontendUrl:"http://localhost:4100",
-  apiUrl:"http://127.0.0.1:3200/api/v1",
-  apiDirectUrl:"http://127.0.0.1:3200/",
+  apiUrl:"http://127.0.0.1:3000/api/v1",
+  apiDirectUrl:"http://127.0.0.1:3000/",
   apiUploadUrl:"https://pickpickgo.s3.ap-southeast-1.amazonaws.com",
-  apiSocketUrl:"http://127.0.0.1:3200/",
+  apiSocketUrl:"http://127.0.0.1:3000/",
   recaptchaSiteKey: '6Le_ytYrAAAAAPtWAIltkJkF4ijMK3jpGVQUEtQ-',
   googleClientId: '524061599462-5ft652kfahvputtng3vr70e43tn741qg.apps.googleusercontent.com', // Replace with your actual Google Client ID
   enableServiceWorker: false,
